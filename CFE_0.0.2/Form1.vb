846d49d1ba6fe81fb428cece529f3b0443361617
MiguelAngel-9k <66149370+MiguelAngel-9k@users.noreply.github.com>
2021-04-27 02:42:13 -0500

    pagina cliente y agregar empleado terminadas

--- CFE_0.0.2/Form1.vb
+++ CFE_0.0.2/Form1.vb
@@ -1,8 +1,30 @@
 ﻿
 Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        log_as_cb.Items.Add("Cliente")
+        log_as_cb.Items.Add("Administrador")
+        log_as_cb.Items.Add("Empleado")
+
+        log_as_cb.SelectedItem = "Empleado"
+    End Sub
     Private Sub login_btn_Click(sender As Object, e As EventArgs) Handles login_btn.Click
         Me.Hide()
         employee_form.Show()
+
+        Dim selected = log_as_cb.SelectedItem.ToString
+
+        If selected = "Cliente" Then
+
+            client_form.Show()
+            Me.Hide()
+        ElseIf selected = "Administrador" Then
+
+            employee_form.Width = 1180
+
+            employee_form.Show()
+            Me.Hide()
+
+        End If
     End Sub
 
     Private Sub close_btn_Click(sender As Object, e As EventArgs) Handles close_btn.Click
